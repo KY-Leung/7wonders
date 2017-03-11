6c10719fda52310448c3eb3d67a494565c7ad497
Yiu Yiu <Yiu Yiu>
2017-03-11 14:10:08 +0800

    Added Adam's edition dated 09/03/17

---
+++ SourceCodes/VirusBusters/VirusBusters/default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Entity.Validation;
+using System.Configuration;
+using System.Data;
+
+namespace VirusBusters
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+
+        protected string lat;
+        protected string lng;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+              lat = "1.3521";
+            lng = "103.8198";
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * from clinic", ConfigurationManager
+        .ConnectionStrings["myDB"].ConnectionString);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+
+            DataTable dt = ds.Tables[0];
+            TextBox1.Text = ds.Tables[0].Rows[0]["lat"].ToString();
+           
+
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+                }
+
+        public string RunCommand(string Query)
+        {
+
+            string connectionString = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand com = new SqlCommand(Query, con);
+            con.Open();
+            string res = com.ExecuteScalar().ToString(); 
+            con.Close();
+            return res;
+        }
+
+
+        public void runReader(string query)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT lat FROM clinic WHERE Id = 1";
+           string results = RunCommand(query);
+            TextBox1.Text = results;
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+       
+    }
+    }
+}
---
+++ SourceCodes/VirusBusters/VirusBusters/map.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VirusBusters
+{
+    public partial class map : System.Web.UI.Page
+    {
+
+        protected string lat;
+        protected string lng;
+        protected int entry =1;
+        protected int count;
+        protected String[] latArray;
+        protected String[] lngArray;
+        protected String[] nameAr;
+
+      
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Text = "haha";
+            SqlDataAdapter da = new SqlDataAdapter("Select * from clinic", ConfigurationManager.ConnectionStrings["myDB"].ConnectionString);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+
+            String query = "Select count(*) from clinic";
+            count = Convert.ToInt32(RunCommand(query));
+             latArray = new String[count];
+             lngArray = new String[count];
+            nameAr = new String[count];
+
+            for (int i=0; i<count; i++)
+            {
+                latArray[i] = ds.Tables[0].Rows[i]["lat"].ToString();
+                lngArray[i] = ds.Tables[0].Rows[i]["long"].ToString();
+                nameAr[i] = ds.Tables[0].Rows[i]["cN"].ToString();
+
+                StringBuilder name = new StringBuilder(nameAr[i]);
+                name.Insert(0, "'");
+                name.Append("'");
+                
+
+                Page.ClientScript.RegisterArrayDeclaration("latArray", ds.Tables[0].Rows[i]["lat"].ToString());
+                Page.ClientScript.RegisterArrayDeclaration("lngArray", ds.Tables[0].Rows[i]["long"].ToString());
+               Page.ClientScript.RegisterArrayDeclaration("nameAr",name.ToString() );
+
+            }
+
+            
+
+           
+
+                 lat = "1.4521";
+            lng = "103.8198";
+
+          
+        }
+
+        public string RunCommand(string Query)
+        {
+
+            string connectionString = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand com = new SqlCommand(Query, con);
+            con.Open();
+            string res = com.ExecuteScalar().ToString();
+            con.Close();
+            return res;
+        }
+
+   
+
+        protected void Button124_Click(object sender, EventArgs e)
+        {
+            
+         
+            string query = "SELECT count(*) FROM clinic ";
+            string rows = RunCommand(query);
+            TextBox1.Text = rows.ToString();
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT count(*) FROM clinic ";
+            string rows = RunCommand(query);
+            TextBox1.Text = rows.ToString();
+            Response.Redirect("test.aspx?value=" + rows);//sending the name to other page
+        }
+
+        protected void Button2_Click(object sender, CommandEventArgs e)
+        {
+
+            TextBox1.Text = e.CommandArgument.ToString();
+
+        }
+
+
+
+      
+
+    
+    }
+}
