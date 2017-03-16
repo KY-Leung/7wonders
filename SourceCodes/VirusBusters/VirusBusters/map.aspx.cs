0b655187acb12bc7f2b01d8a278dbebe62f910a6
KY-Leung <kaiyiu_blue@hotmail.com>
2017-03-16 18:43:04 +0800

    Resolved map by adding ASP.NET form

--- SourceCodes/VirusBusters/VirusBusters/map.aspx.cs
+++ SourceCodes/VirusBusters/VirusBusters/map.aspx.cs
@@ -13,30 +13,13 @@ namespace VirusBusters
 {
     public partial class map : System.Web.UI.Page
     {
-
         protected string lat;
         protected string lng;
-        protected int entry =1;
+        protected int entry = 1;
         protected int count;
         protected String[] latArray;
         protected String[] lngArray;
         protected String[] nameAr;
-
-        protected void Page_PreInit(object sender, EventArgs e)
-        {
-            bool isLoggedIn = (System.Web.HttpContext.Current.User != null) && System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
-            if (isLoggedIn)
-            {
-                if (string.Equals(HttpContext.Current.User.Identity.Name, "admin", StringComparison.CurrentCultureIgnoreCase)) //to be changed to role
-                {
-                    this.MasterPageFile = "~/admin.master";
-                }
-                else
-                {
-                    this.MasterPageFile = "~/public.master";
-                }
-            }
-        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -44,46 +27,32 @@ namespace VirusBusters
             DataSet ds = new DataSet();
             da.Fill(ds);
             DataTable dt = ds.Tables[0];
-
             String query = "Select count(*) from clinic";
             count = Convert.ToInt32(RunCommand(query));
             latArray = new String[count];
             lngArray = new String[count];
             nameAr = new String[count];
 
-            for (int i=0; i<count; i++)
+            for (int i = 0; i < count; i++)
             {
                 latArray[i] = ds.Tables[0].Rows[i]["lat"].ToString();
                 lngArray[i] = ds.Tables[0].Rows[i]["long"].ToString();
                 nameAr[i] = ds.Tables[0].Rows[i]["cN"].ToString();
-
                 StringBuilder name = new StringBuilder(nameAr[i]);
                 name.Insert(0, "'");
                 name.Append("'");
-                
-
                 Page.ClientScript.RegisterArrayDeclaration("latArray", ds.Tables[0].Rows[i]["lat"].ToString());
                 Page.ClientScript.RegisterArrayDeclaration("lngArray", ds.Tables[0].Rows[i]["long"].ToString());
-                Page.ClientScript.RegisterArrayDeclaration("nameAr",name.ToString() );
-
+                Page.ClientScript.RegisterArrayDeclaration("nameAr", name.ToString());
             }
-
-            
-
-           
-
-                 lat = "1.4521";
+            lat = "1.4521";
             lng = "103.8198";
-
-          
         }
 
         public string RunCommand(string Query)
         {
-
             string connectionString = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
             SqlConnection con = new SqlConnection(connectionString);
-
             SqlCommand com = new SqlCommand(Query, con);
             con.Open();
             string res = com.ExecuteScalar().ToString();
@@ -91,16 +60,10 @@ namespace VirusBusters
             return res;
         }
 
-   
-
         protected void Button124_Click(object sender, EventArgs e)
         {
-            
-         
             string query = "SELECT count(*) FROM clinic ";
             string rows = RunCommand(query);
-
-
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -112,14 +75,6 @@ namespace VirusBusters
 
         protected void Button2_Click(object sender, CommandEventArgs e)
         {
-
-
         }
-
-
-
-      
-
-    
     }
 }
